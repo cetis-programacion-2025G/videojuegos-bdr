7978e96d40effe41ed4b0f791a85b64fe58638be
JFco Díaz <me@pakodiaz.dev>
2026-05-25 15:52:01 -0600

    style: expandir todos los ifs de una línea a bloques multi-línea

--- db/videojuegos/buscarVideojuego.php
+++ db/videojuegos/buscarVideojuego.php
@@ -1,7 +1,9 @@
 <?php
 function buscarVideojuego(&$datos, $id) {
     for ($i = 0; $i < count($datos['videojuegos']); $i++) {
-        if ($datos['videojuegos'][$i]['id'] === $id) return $datos['videojuegos'][$i];
+        if ($datos['videojuegos'][$i]['id'] === $id) {
+            return $datos['videojuegos'][$i];
+        }
     }
     return null;
 }

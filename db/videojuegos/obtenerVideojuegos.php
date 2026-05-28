8da0ce5ab2c7ee587b9917bd9e9bbb52eefb7ffc
Dayis <dayiscobain@gmail.com>
2026-05-27 19:10:26 -0600

    obtener videojuego

--- db/videojuegos/obtenerVideojuegos.php
+++ db/videojuegos/obtenerVideojuegos.php
@@ -2,5 +2,13 @@
 // TODO (MySQL):
 // SELECT * FROM videojuegos ORDER BY titulo
 function obtenerVideojuegos(&$datos) {
+    $conn = conectar();
+    $sql ="SELECT * FROM videojuegos";
+    
+    $resultado = $conn->query($sql);
+    $videojuegos = [];
+    while ($fila = $resultado->fetch_assoc()) {
+        $videojuegos[] = $fila;
+    }
     return $datos['videojuegos'];
 }
